ce738946cfcfdb9b5d9a3efe077be3168d9228de
Raiyan Siraj Akib <developerraiyan@gmail.com>
2021-04-29 23:32:42 +0600

    Added Headers Support

--- request.js
+++ request.js
@@ -1,31 +1,45 @@
 module.exports = class AppReq {
-    constructor(request) {
-        this.request = request
-        this.params = {}
-    }
+  constructor(request) {
+    this.request = request;
+    this.params = {};
+  }
 
-    async body() {
-        try {
-            return await this.request.json()
-        } catch (error) {
-            return {} // cases when body is null, but still json in content header
+  async body() {
+    try {
+      return await this.request.json();
+    } catch (error) {
+      return {}; // cases when body is null, but still json in content header
+    }
+  }
+
+  query() {
+    try {
+      let query = {};
+      let queryString = this.request.url.split("?")[1];
+
+      queryString.split("&").forEach((el) => {
+        const temp = el.split("=");
+        if (temp.length === 2) {
+          query[temp[0]] = temp[1];
         }
+      });
+      return query;
+    } catch (error) {
+      return {};
     }
+  }
 
-    query() {
-        try {
-            let query = {}
-            let queryString = this.request.url.split('?')[1]
-
-            queryString.split('&').forEach(el => {
-                const temp = el.split('=')
-                if (temp.length === 2) {
-                    query[temp[0]] = temp[1]
-                }
-            })
-            return query
-        } catch (error) {
-            return {}
-        }
+  /**
+   * Takes name of the header and returns value of the header
+   * @param {string} name Header Name
+   * @returns {string} Value of Header
+   */
+  async header(name) {
+    try {
+      const header = await this.request.headers.get(name);
+      return header;
+    } catch (error) {
+      return {};
     }
-}
+  }
+};
--- response.js
+++ response.js
@@ -1,14 +1,17 @@
 module.exports = class AppRes {
-    constructor(request) {
+  constructor(request) {}
 
-    }
-
-    send(data, status = 200) {
-        return new Response(JSON.stringify(data), {
-            status,
-            headers: {
-                "content-type": "application/json"
-            }
-        })
-    }
-}
+  /**
+   * Send Response with data, headers and status code
+   * @param {any} data data sent by user
+   * @param {Object} headers headers object sent by user, default {'content-type': 'application/json'}
+   * @param {boolean} status status for the response, default 200
+   * @returns
+   */
+  send(data, headers = { "content-type": "application/json" }, status = 200) {
+    return new Response(JSON.stringify(data), {
+      status,
+      headers,
+    });
+  }
+};
